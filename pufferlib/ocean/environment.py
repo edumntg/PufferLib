7e6e5c39b7ed730ecf49cea05ab0902ce6cccadc
Eduardo Montilva <edumntg@gmail.com>
2025-07-20 23:34:49 -0400

    ini file and register at ocean/environment.py

--- pufferlib/ocean/environment.py
+++ pufferlib/ocean/environment.py
@@ -155,6 +155,7 @@ MAKE_FUNCTIONS = {
     'asteroids': 'Asteroids',
     'spaces': make_spaces,
     'multiagent': make_multiagent,
+    'maglev': 'MagLev'
 }
 
 def env_creator(name='squared', *args, **kwargs):
